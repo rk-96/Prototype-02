83eb751e591d5ac0d4f20c1ad18be4ef3b039a6f
bluehouse <kicvong@gmail.com>
2020-11-02 22:16:26 +0700

    Object Pooling

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,17 +16,20 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
         }
         else if (transform.position.z < lowerBound)
         {
-            Debug.Log("GAME OVER!");
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
     }
 }
-
